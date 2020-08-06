88cb34cc7f36585647eaddd74c7f9fbb063687a9
dengyu <313187515@qq.com>
2020-08-06 12:01:06 +0800

    账号密码加入验证

--- qiushibaike/pages/login/login.vue
+++ qiushibaike/pages/login/login.vue
@@ -9,55 +9,47 @@
 		<!-- 输入框+按钮 -->
 		<view class="body">
 			<template v-if="loginStatus">
-				<input type="text"
-				class="uni-input common-input"
-				placeholder="昵称/手机号/邮箱" v-model="userName"/>
-				
+				<input type="text" class="uni-input common-input" placeholder="昵称/手机号/邮箱" v-model="userName" />
+
 				<view class="login-input-box">
-					<input type="text"
-					class="uni-input common-input forget-input"
-					placeholder="请输入密码" v-model="password" />
+					<input type="text" class="uni-input common-input forget-input" placeholder="请输入密码" v-model="password" />
 					<view class="forget u-f-ajc login-font-color">忘记密码</view>
 				</view>
 			</template>
-			
+
 			<template v-else>
 				<view class="login-input-box">
 					<view class="phone u-f-ajc">+86</view>
-					<input type="text"
-					class="uni-input common-input phone-input"
-					placeholder="手机号" v-model="phoneNum" />
+					<input type="text" class="uni-input common-input phone-input" placeholder="手机号" v-model="phoneNum" />
 				</view>
 				<view class="login-input-box">
-					<input type="text"
-					class="uni-input common-input forget-input"
-					placeholder="请输入验证码" v-model="checkNum" />
-					<view class="forget u-f-ajc login-font-color yanzhengma">
-						<view class="u-f-ajc">获取验证码</view>
+					<input type="text" class="uni-input common-input forget-input" placeholder="请输入验证码" v-model="checkNum" />
+					<view class="forget u-f-ajc login-font-color yanzhengma" @tap="getCheckNum">
+						<view class="u-f-ajc">{{!checkTime? '获取验证码':checkTime+'s'}}</view>
 					</view>
 				</view>
 			</template>
-			
-			
-			<button class="user-set-btn" 
-			:loading="loading" :class="{'user-set-btn-disable':disabled}" 
-			type="primary" @tap="submit" :disabled="disabled">登录</button>
+
+
+			<button class="user-set-btn" :loading="loading" :class="{'user-set-btn-disable':disabled}" type="primary" @tap="submit"
+			 :disabled="disabled">登录</button>
 		</view>
-		
+
 		<!-- 登录状态切换 -->
 		<view class="login-status u-f-ajc login-padding login-font-color" @tap="changeLoginStatus">
-			{{loginStatus? '验证码登录':'账号密码登录'}}<view class="icon iconfont icon-jinru login-font-color"></view>
+			{{loginStatus? '验证码登录':'账号密码登录'}}
+			<view class="icon iconfont icon-jinru login-font-color"></view>
 		</view>
-		
+
 		<!-- 第三方登陆 -->
 		<view class="other-login-title u-f-ajc login-padding login-font-color">第三方登录</view>
 		<other-login></other-login>
-		
+
 		<!-- 协议 -->
 		<view class="login-rule u-f-ajc login-padding login-font-color">
 			注册即代表您同意<view>《XXX协议》</view>
 		</view>
-		
+
 	</view>
 </template>
 
@@ -65,64 +57,97 @@
 	import uniStatusBar from "../../components/uni-status-bar/uni-status-bar.vue";
 	import otherLogin from "../../components/home/other-login.vue";
 	export default {
-		components:{
+		components: {
 			uniStatusBar,
 			otherLogin
 		},
 		data() {
 			return {
-				disabled:true,
-				loading:false,
-				loginStatus: true ,// true 代表账号密码登录 false代码验证码登录
+				disabled: true,
+				loading: false,
+				loginStatus: true, // true 代表账号密码登录 false代码验证码登录
 				userName: '',
 				password: '',
 				phoneNum: '',
-				checkNum: ''
+				checkNum: '',
+				checkTime: 0
 			}
 		},
-		watch:{
-			userName(){
+		watch: {
+			userName() {
 				this.changeBtnStatus()
 			},
-			password(){
+			password() {
 				this.changeBtnStatus()
 			},
-			phoneNum(){
+			phoneNum() {
 				this.changeBtnStatus()
 			},
-			checkNum(){
+			checkNum() {
 				this.changeBtnStatus()
 			},
 		},
 		methods: {
+			// 验证手机号
+			checkPhone(){
+				//手机号正则
+				var mPattern = /^1[34578]\d{9}$/; 
+				if(mPattern.test(this.phoneNum)){
+					return true
+				}
+				if(!mPattern.test(this.phoneNum)){
+					uni.showToast({
+						title: '请输入正确的手机号码！',
+						icon:'none'
+					});
+					return false
+				}
+				
+			},
+			// 获取验证码
+			getCheckNum() {
+				if (this.checkTime > 0) {
+					return
+				};
+				if(!this.checkPhone()) return
+				this.checkTime = 10
+				this.timer = setInterval(() => {
+					this.checkTime--
+					if (this.checkTime < 1) {
+						clearInterval(this.timer)
+						this.checkTime = 0
+					}
+				}, 1000)
+			},
 			// 切换登录类型
-			changeLoginStatus(){
+			changeLoginStatus() {
 				this.initData()
 				this.loginStatus = !this.loginStatus
 			},
 			// 初始化登录表单数值
-			initData(){
+			initData() {
 				this.userName = ''
 				this.password = ''
 				this.phoneNum = ''
 				this.checkNum = ''
 			},
 			// 提交按钮点击状态
-			changeBtnStatus(){
-				if((this.userName.trim()&&this.password.trim())||(this.phoneNum.trim()&&this.checkNum.trim())){
+			changeBtnStatus() {
+				if ((this.userName.trim() && this.password.trim()) || (this.phoneNum.trim() && this.checkNum.trim())) {
 					this.disabled = false
 					return
 				}
 				this.disabled = true
 			},
 			// 返回上一步
-			back(){
+			back() {
 				uni.navigateBack({
 					delta: 1
 				});
 			},
 			// 提交登录
-			submit(){
+			submit() {
+				if(!this.checkPhone()) return
 				console.log("提交登录")
 			}
 		}
@@ -130,65 +155,88 @@
 </script>
 
 <style>
-@import "../../common/form.css";
-.login-font-color{ color: #BBBBBB; }
-.login-padding{ padding: 20upx 0; }
-.icon-fanhui{
-	position: fixed;
-	top: 60upx;
-	left: 30upx;
-	font-size: 40upx;
-	font-weight: bold;
-	color: #332F0A;
-	z-index: 99999;
-}
-.loginhead{ width: 100%; }
-.other-login-title{
-	position: relative;
-}
-.other-login-title:before,.other-login-title:after{
-	content: "";
-	position: absolute;
-	background: #BBBBBB;
-	height: 1upx;
-	width: 100upx;
-	top: 50%;
-}
-.other-login-title:before{
-	left: 25%;
-}
-.other-login-title:after{
-	right: 25%;
-}
-.login-input-box{
-	position: relative;
-}
-.login-input-box .forget-input{
-	padding-right: 150upx;
-}
-.login-input-box .forget,.login-input-box .phone{
-	position: absolute;
-	top: 0;
-	height: 100%;
-	z-index: 100;
-}
-.login-input-box .forget{
-	width: 150upx;
-	right: 0;
-}
-.login-input-box .phone{
-	width:100upx;
-	left: 0;
-	font-weight: bold;
-}
-.login-input-box .phone-input{
-	padding-left: 100upx;
-}
-.yanzhengma view{
-	background: #EEEEEE;
-	border-radius: 10upx;
-	font-size: 25upx;
-	width: 150upx;
-	padding: 10upx 0;
-}
+	@import "../../common/form.css";
+
+	.login-font-color {
+		color: #BBBBBB;
+	}
+
+	.login-padding {
+		padding: 20upx 0;
+	}
+
+	.icon-fanhui {
+		position: fixed;
+		top: 60upx;
+		left: 30upx;
+		font-size: 40upx;
+		font-weight: bold;
+		color: #332F0A;
+		z-index: 99999;
+	}
+
+	.loginhead {
+		width: 100%;
+	}
+
+	.other-login-title {
+		position: relative;
+	}
+
+	.other-login-title:before,
+	.other-login-title:after {
+		content: "";
+		position: absolute;
+		background: #BBBBBB;
+		height: 1upx;
+		width: 100upx;
+		top: 50%;
+	}
+
+	.other-login-title:before {
+		left: 25%;
+	}
+
+	.other-login-title:after {
+		right: 25%;
+	}
+
+	.login-input-box {
+		position: relative;
+	}
+
+	.login-input-box .forget-input {
+		padding-right: 150upx;
+	}
+
+	.login-input-box .forget,
+	.login-input-box .phone {
+		position: absolute;
+		top: 0;
+		height: 100%;
+		z-index: 100;
+	}
+
+	.login-input-box .forget {
+		width: 150upx;
+		right: 0;
+	}
+
+	.login-input-box .phone {
+		width: 100upx;
+		left: 0;
+		font-weight: bold;
+	}
+
+	.login-input-box .phone-input {
+		padding-left: 100upx;
+	}
+
+	.yanzhengma view {
+		background: #EEEEEE;
+		border-radius: 10upx;
+		font-size: 25upx;
+		width: 150upx;
+		padding: 10upx 0;
+	}
 </style>
